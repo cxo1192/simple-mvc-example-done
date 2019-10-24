7cfd550dfbf913f9b63bc52f5b3ef003f1af4db9
cxo1192 <cxo1192@g.rit.edu>
2019-10-24 18:14:49 -0400

    started simple models ice

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,6 +3,8 @@ const models = require('../models');
 
 // get the Cat model
 const Cat = models.Cat.CatModel;
+// get the Dog model
+const Dog = models.Dog.DogModel;
 
 // default fake data so that we have something to work with until we make a real Cat
 const defaultData = {
@@ -10,8 +12,15 @@ const defaultData = {
   bedsOwned: 0,
 };
 
+const defaultDogData = {
+  name: 'unknown',
+  breed: 'unknown',
+  age: 0,
+};
+
 // object for us to keep track of the last Cat we made and dynamically update it sometimes
 let lastAdded = new Cat(defaultData);
+let lastAddedDog = new Dog(defaultDogData);
 
 // function to handle requests to the main page
 // controller functions in Express receive the full HTTP request
@@ -103,13 +112,23 @@ const hostPage2 = (req, res) => {
 // controller functions in Express receive the full HTTP request
 // and a pre-filled out response object to send
 const hostPage3 = (req, res) => {
-    // res.render takes a name of a page to render.
-    // These must be in the folder you specified as views in your main app.js file
-    // Additionally, you don't need .jade because you registered the file type
-    // in the app.js as jade. Calling res.render('index')
-    // actually calls index.jade. A second parameter of JSON can be passed
-    // into the jade to be used as variables with #{varName}
+  // res.render takes a name of a page to render.
+  // These must be in the folder you specified as views in your main app.js file
+  // Additionally, you don't need .jade because you registered the file type
+  // in the app.js as jade. Calling res.render('index')
+  // actually calls index.jade. A second parameter of JSON can be passed
+  // into the jade to be used as variables with #{varName}
   res.render('page3');
+};
+
+const hostPage4 = (req, res) => {
+  // res.render takes a name of a page to render.
+  // These must be in the folder you specified as views in your main app.js file
+  // Additionally, you don't need .jade because you registered the file type
+  // in the app.js as jade. Calling res.render('index')
+  // actually calls index.jade. A second parameter of JSON can be passed
+  // into the jade to be used as variables with #{varName}
+  res.render('page4');
 };
 
 // function to handle get request to send the name
@@ -158,6 +177,54 @@ const setName = (req, res) => {
     lastAdded = newCat;
     // return success
     res.json({ name: lastAdded.name, beds: lastAdded.bedsOwned });
+  });
+
+  // if error, return it
+  savePromise.catch((err) => res.json({ err }));
+
+  return res;
+};
+
+
+// /create dog
+const setDogName = (req, res) => {
+  // check if the required fields exist
+  // normally you would also perform validation
+  // to know if the data they sent you was real
+  if (!req.body.firstname || !req.body.lastname || !req.body.breed || !req.body.age) {
+    // if not respond with a 400 error
+    // (either through json or a web page depending on the client dev)
+    return res.status(400).json({ error: 'firstname,lastname, breed, and age are all required' });
+  }
+
+  // if required fields are good, then set name
+  const name = `${req.body.firstname} ${req.body.lastname}`;
+
+  // dummy JSON to insert into database
+  const dogData = {
+    name,
+    breed: req.body.breed,
+    age: req.body.age,
+    createdDate: Date.now,
+  };
+
+  // create a new object of CatModel with the object to save
+  const newDog = new Dog(dogData);
+
+  // create new save promise for the database
+  const savePromise = newDog.save();
+
+  savePromise.then(() => {
+    // set the lastAdded cat to our newest cat object.
+    // This way we can update it dynamically
+    lastAddedDog = newDog;
+    // return success
+    res.json({
+      name: lastAddedDog.name,
+      breed: lastAddedDog.breed,
+      age: lastAddedDog.age,
+      createdDate: lastAddedDog.createdDate,
+    });
   });
 
   // if error, return it
@@ -232,6 +299,48 @@ const updateLast = (req, res) => {
   savePromise.catch((err) => res.json({ err }));
 };
 
+
+// find dog and increase age
+const searchDogNameAndUpdate = (req, res) => {
+  if (!req.query.nameSearch || !req.query.ageAdd) {
+    return res.json({ error: 'Name, and Age is required' });
+  }
+
+  return Dog.findByName(req.query.nameSearch, (err, doc) => {
+    // errs, handle them
+    if (err) {
+      return res.json({ err }); // if error, return it
+    }
+
+    // if no matches, let them know
+    // (does not necessarily have to be an error since technically it worked correctly)
+    if (!doc) {
+      return res.json({ error: 'No dogs with that name found' });
+    }
+
+    // if a match, send the match back
+    // return res.json({ name: doc.name, beds: doc.bedsOwned });
+    // doc.age += req.query.ageAdd;
+    const newDogData = {
+      name: doc.name,
+      breed: doc.breed,
+      age: doc.age + req.query.addAge,
+      createdDate: doc.createdDate,
+    };
+
+    const newDoc = new Dog(newDogData);
+
+    const savePromise = newDoc.save();
+
+    // send back the name as a success for now
+    savePromise.then(() => res.json({ name: doc.name, breed: doc.breed, age: doc.age }));
+
+    // if save error, just return an error for now
+    savePromise.catch(res.json({ err }));
+    return res;
+  });
+};
+
 // function to handle a request to any non-real resources (404)
 // controller functions in Express receive the full HTTP request
 // and get a pre-filled out response object to send
@@ -253,10 +362,13 @@ module.exports = {
   page1: hostPage1,
   page2: hostPage2,
   page3: hostPage3,
+  page4: hostPage4,
   readCat,
   getName,
   setName,
   updateLast,
   searchName,
   notFound,
+  setDogName,
+  searchDogNameAndUpdate,
 };
---
+++ server/models/Dog.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+let DogModel = {};
+const DogSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+
+  breed: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  age: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+
+  createdDate: {
+    type: Number,
+    required: true,
+  },
+
+});
+
+DogSchema.statics.findByName = (name, callback) => {
+  const search = {
+    name,
+  };
+
+  return DogModel.findOne(search, callback);
+};
+
+
+//   DogSchema.statics.increaseAgeByName(name,age,callback) => {
+//     let dog = DogModel.findByName()
+//   };
+
+
+DogModel = mongoose.model('Dog', DogSchema);
+
+
+module.exports.DogModel = DogModel;
+module.exports.DogSchema = DogSchema;
